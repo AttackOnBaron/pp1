155e549a178fdaaeffe34416f71ad8a1fb18672c
AttackOnBaron <35652516+AttackOnBaron@users.noreply.github.com>
2019-04-09 00:00:51 -0500

    Update q11.c

--- q11.c
+++ q11.c
@@ -25,6 +25,7 @@ mysecond()
 }
 
 /* ---------------------------------------- FIND_MIN */
+//Regular
 void *find_min(void *list_ptr)
 {
 	/* vars */
@@ -50,6 +51,27 @@ void *find_min(void *list_ptr)
 	}
 	pthread_mutex_unlock(&minimum_value_lock);
 
+	pthread_exit(0);
+}
+	
+//Read-Write locks
+void *find_min_rw(void *list_ptr) {
+	int *partial_list_pointer, my_min, i;
+	my_min = MIN_INT;
+	partial_list_pointer = (int *) list_ptr;
+	for (i = 0; i < partial_list_size; i++)
+  	if (partial_list_pointer[i] < my_min)
+	my_min = partial_list_pointer[i];
+	/* lock the mutex associated with minimum_value and
+	update the variable as required */
+	mylib_rwlock_rlock(&read_write_lock);
+	if (my_min < minimum_value) {
+		mylib_rwlock_unlock(&read_write_lock);
+		mylib_rwlock_wlock(&read_write_lock);
+		minimum_value = my_min;
+	}
+/* and unlock the mutex */
+	mylib_rwlock_unlock(&read_write_lock);
 	pthread_exit(0);
 }
 
