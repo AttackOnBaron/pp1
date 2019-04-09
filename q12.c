061bfb579586da61cb9d8d5236de618c3e613bc5
AttackOnBaron <35652516+AttackOnBaron@users.noreply.github.com>
2019-04-09 00:20:15 -0500

    Update q12.c

--- q12.c
+++ q12.c
@@ -1,170 +1,165 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <limits.h>
-#include <pthread.h>
+#include<pthread.h>
+#include<stdio.h>
+#include<sys/time.h>				// header file inclusion
+#include<stdlib.h>
 
 
-/* ---------------------------------------- GLOBALS */
-pthread_mutex_t	minimum_value_lock;
+
+#define MIN_INT 0				// define necessary values
+#define MAXTHREADS 16
+
+void *find_min_rwlock(void *) ;
+void checkResults(char *string, int rc);
+
+pthread_rwlock_t       rwlock;                   // declaration of read-write lock variable
+
+long int partial_list_size;
 int minimum_value;
-long partial_list_size;
+long int *list;                                     // declare global variables
+long int NumElements;
+int NumThreads;
 
-/* ---------------------------------------- MYSECOND */
-static double 
-mysecond()
+int main (int argc,char * argv[]) 
 {
-	struct timeval	tp;
-	struct timezone	tzp;
-	int i = 0;
+ 
+        pthread_t       *threads;
+        pthread_attr_t  pta;
+        int             iteration,THREADS,ret_count;
+	char 		* CLASS;
 
-	i = gettimeofday(&tp, &tzp);
-	return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
-}
+	double          time_start, time_end;
+        struct          timeval tv;                                // variables required for timing
+        struct          timezone tz;
 
-/* ---------------------------------------- FIND_MIN */
+        int counter;        
+        printf("\n\t\t Objective : To compute the minimum of a list of numbers using read-write locks \n ");
+       
+         if( argc != 3 )                                                 // check command line arguments
+	 {
+	    printf("\t\t Very Few Arguments\n ");
+            printf("\t\t Syntax : exec <NumElements> <Threads>\n");		
+            exit(-1);
+         }												// user input
+         else {
+            NumElements =atoi( argv[1]);
+            THREADS = atoi(argv[2]);
+         }
+
+        NumThreads = THREADS;
+        printf("\n\t\t Array Size  :  %ld",NumElements);
+        printf("\n\t\t Threads     :  %d",NumThreads);
+        printf("\n");
+
+        if (NumThreads < 1 )
+        {
+           printf("\n Number of threads must be greater than zero. Aborting ...\n");
+           return 0;
+        }
+
+        if ((NumThreads != 1) && (NumThreads != 2) && (NumThreads != 4) && (NumThreads != 8))
+        {
+           printf("\n Number of Threads must be 1 or 2 or 4 or 8. Aborting ...\n");
+           return 0;
+        }
+
+        if ( ( NumElements % NumThreads ) != 0 )
+        {
+           printf("\n Number of threads not a factor of Integer List size. Aborting.\n");
+ 	   return 0 ;
+        }
+
+
+        partial_list_size = NumElements / NumThreads;                             // partial list size for each thread
+
+        list = (long int *)malloc(sizeof(long int) * NumElements);
+        
+         
+        for(counter = 0 ; counter < NumElements ; counter++)
+        {
+            srand48((unsigned int)NumElements);							// random number generation
+            list[counter] = (rand()%1000)+1.0;
+        }
+         
+        threads = (pthread_t *)malloc(sizeof(pthread_t)*NumThreads);       
+
+        minimum_value = list[0];         
+
+       
+        ret_count = pthread_rwlock_init(&rwlock, NULL);						// initialise rwlock
+	checkResults("pthread_rwlock_init()\n", ret_count);
+
+	ret_count=pthread_attr_init(&pta);
+        checkResults("pthread_attr_init()\n", ret_count);
+
+        pthread_attr_setscope(&pta,PTHREAD_SCOPE_SYSTEM);
+
+	gettimeofday(&tv, &tz);
+        time_start = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
+
 	
-//Read-Write locks
-void *find_min_rw(void *list_ptr) {
-	int *partial_list_pointer, my_min, i;
-	my_min = MIN_INT;
-	partial_list_pointer = (int *) list_ptr;
-	for (i = 0; i < partial_list_size; i++)
-  	if (partial_list_pointer[i] < my_min)
-	my_min = partial_list_pointer[i];
-	/* lock the mutex associated with minimum_value and
-	update the variable as required */
-	mylib_rwlock_rlock(&read_write_lock);
-	if (my_min < minimum_value) {
-		mylib_rwlock_unlock(&read_write_lock);
-		mylib_rwlock_wlock(&read_write_lock);
-		minimum_value = my_min;
+	for(counter = 0 ; counter < NumThreads ; counter++)
+	{
+           ret_count=pthread_create(&threads[counter],&pta,(void *(*) (void *)) find_min_rwlock,(void *) (counter+1));     // call find_min_rwlock function
+	   checkResults("pthread_create()\n", ret_count);
 	}
-/* and unlock the mutex */
-	mylib_rwlock_unlock(&read_write_lock);
-	pthread_exit(0);
-}
 
-/* ---------------------------------------- MAIN */
-int main(int argc, char **argv)
+	gettimeofday(&tv, &tz);
+        time_end = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
+
+
+      	
+        for(counter = 0 ; counter < NumThreads ; counter++)
+	{
+           ret_count=pthread_join(threads[counter],NULL);
+	   checkResults("pthread_join()\n", ret_count);
+        }
+        ret_count=pthread_attr_destroy(&pta);
+	checkResults("pthread_attr_destroy()\n", ret_count);
+
+        printf("\n\t\t..........................................................................\n");
+        printf("\n\t\t Minimum Value found in the Integer list     :  %d",minimum_value);
+	printf("\n\t\t Time Taken in Seconds  (T)                  :  %lf Seconds",( time_end - time_start));
+        printf("\n\t\t..........................................................................\n");
+        
+        free(list);
+        free(threads);
+        return 0;
+
+ }
+ void *find_min_rwlock(void * myid )                             // function to find min value
 {
-	/* vars */
-	int nt = 10;
-	int ret = 0;
-	int i = 0;
-	long l = 0;
-	long cur = 0;
-	int seed = 10;
-	long nelems = 100000000;
-	double start = 0.;
-	double end = 0.;
-	int *list = NULL;
-	pthread_t *tids = NULL;
-	void *res = NULL;
-	/* ---- */
-	printf("Usage : %s%s", argv[0], " -S <seed> -N <num_elems> -T <num_threads> -h\n");
-	printf("Default number of threads = %d\nDefault number of elements = %ld\nDefault seed value = %d\n", nt, nelems, seed);
-	/* parse the command line args */
-	while ((ret = getopt(argc, argv, "S:T:N:h")) != -1) {
-		switch (ret) {
-		case 'h':
-			printf("Usage : %s%s", argv[0], " -S <seed> -N <num_elems> -T <num_threads> -h\n");
-			return 0;
-			break;
-		case 'S':
-			seed = atoi(optarg);
-			break;
-		case 'N':
-			nelems = atol(optarg);
-			break;
-		case 'T':
-			nt = atoi(optarg);
-			break;
-		case '?':
-		default:
-			printf("Unknown option : %s%s",
-			       argv[0], " -S <seed> -N <num_elems> -T <num_threads> -h\n");
-			return -1;
-			break;
-		}
-	}
 
-	/* sanity check */
-	if (nt < 1) {
-		printf("error : not enough threads\n");
-		return -1;
-	}
-	if (nelems <= (long)(nt)) {
-		printf("error : not enough elements\n");
-		return -1;
-	}
-	minimum_value = INT_MAX;
+     int my_min;
+     long int counter;
 
-	/* init the mutex */
-	pthread_mutex_init(&minimum_value_lock, NULL);
+     int myId = (int)myid;
 
-	/* init lists, list_ptr, partial_list_size */
-	list = malloc(sizeof(int) * nelems);
-	if (list == NULL) {
-		printf("Error : could not init the list\n");
-		return -1;
-	}
-	tids = malloc(sizeof(pthread_t) * nt);
-	if (tids == NULL) {
-		printf("Error : could not init the tids\n");
-		return -1;
-	}
-	srand(seed);
-	for (l = 0; l < nelems; l++) {
-		list[l] = (long)(rand());
-	}
+     my_min = list[(myId-1)*partial_list_size];
 
-	if (nt == 1) {
-		partial_list_size = nelems;
-	} else {
-		partial_list_size = (nelems / (long)(nt)) + (nelems % (long)(nt));
-	}
+     for (counter = ((myId - 1) * partial_list_size); counter <= ((myId * partial_list_size) - 1); counter++){
+        if (list[counter] < my_min)
+            my_min = list[counter];
+     }
 
 
-	start = mysecond();
-	/* create threads */
-	for (i = 0; i < nt; i++) {
-		if (pthread_create(&tids[i], NULL, &find_min, &list[cur]) != 0) {
-			printf("Error : pthread_create failed on spawning thread %d\n", i);
-			return -1;
-		}
-		cur += partial_list_size;
-
-		/*
-		 * we do this check in order to ensure that our threads
-		 * down't go out of bounds of the list
-		 * 
-		 */
-		if ((cur + partial_list_size) > nelems) {
-			cur = nelems - partial_list_size;
-		}
-	}
-
-	/* join threads */
-	for (i = 0; i < nt; i++) {
-		if (pthread_join(tids[i], &res) != 0) {
-			printf("Error : pthread_join failed on joining thread %d\n", i);
-			return -1;
-		}
-	}
-
-	end = mysecond();
-
-	printf("Minimum value found: %d\n", minimum_value);
-	printf("Runtime of %d threads = %f seconds\n", nt, (end - start));
-
-	free(list);
-	free(tids);
-	tids = NULL;
-	list = NULL;
-
-	return 0;
+    pthread_rwlock_rdlock(&rwlock) ;					// acquire read lock
+    if (my_min < minimum_value)
+    {
+       pthread_rwlock_unlock(&rwlock);
+       pthread_rwlock_wrlock(&rwlock);				// if condition is true then only acquire write lock and update variable
+       minimum_value = my_min;
+    }
+    pthread_rwlock_unlock(&rwlock) ;		// unlock rwlock
+   
+    pthread_exit(NULL);
 }
-/* EOF */
+
+void checkResults(char *string, int rc)                            // check return values from pthread APIs
+{
+        if (rc)
+  {
+    printf("Error on : %s, rc=%d",string, rc);
+    exit(0);
+  }
+  return;
+}
